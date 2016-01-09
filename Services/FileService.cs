5427fcde217e76973acddd5d592484e1feafe860
Andrii Grondzal <andriigrondzal@outlook.com>
2016-01-09 17:45:21 +0200

    Fix loading Kohonen network on startup

---
+++ Services/FileService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+
+namespace Services
+{
+    public class FileService
+    {
+        private readonly SettingsService _settingsService = new SettingsService();
+
+        public string GetImageFilePath()
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                CheckFileExists = true,
+                AddExtension = true,
+                Multiselect = false,
+                Filter = "JPEG images|*.jpg; *.jpeg; *.jpe; *.jif; *.jfif; *.jfi|" +
+                        "Bitmap images|*.bmp|GIF images|*.gif|PNG images|" +
+                        "*.png|TIFF images|*.tiff; *.tif|All files|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() != true) { return null; }
+
+            string filePath = openFileDialog.FileName;
+
+            return filePath;
+        }
+
+        public BitmapImage GetBitmapFromPath(string imagePath)
+        {
+            var imageToReturn = new BitmapImage();
+
+            try
+            {
+                imageToReturn = new BitmapImage(new Uri(imagePath));
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("There was an error during opening the image." +
+                                "Please check the path.");
+            }
+
+            return imageToReturn;
+        }
+
+        public BitmapImage GetBitmapFileFromDisk()
+        {
+            return GetBitmapFromPath(GetImageFilePath());
+        }
+
+        public void SaveBitmapImageForRecognitionToParameters()
+        {
+            Parameters.LoadedBitmapImageToRecognize = GetBitmapFileFromDisk();
+        }
+
+        public BitmapImage GetKononenNetworkImageFromDefaultPath()
+        {
+            var networkImage = new BitmapImage();
+            string pathToImage = Directory.GetCurrentDirectory() 
+                + "\\" 
+                + _settingsService.GetSettingValueByKey("PathToKohonenNetwork");
+
+            try
+            {
+                networkImage = new BitmapImage(new Uri(pathToImage, UriKind.Absolute));
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("There was an error during opening the image." +
+                                "Please check the path.");
+            }
+
+            return networkImage;
+        }
+    }
+}
---
+++ Services/SettingsService.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Configuration;
+
+namespace Services
+{
+    public class SettingsService
+    {
+        public string GetSettingValueByKey(string key)
+        {
+            string settingValue = "";
+
+            try
+            {
+                settingValue = ConfigurationManager.AppSettings[key];
+            }
+            catch (ConfigurationErrorsException)
+            {
+                MessageBox.Show("Error reading app settings");
+            }
+            
+            return settingValue;
+        }
+    }
+}
